d10b44b46a96856cbf183b30217536c5329ec24a
voisnoa <markzlopp@gmail.com>
2025-01-24 09:55:59 +0530

    Connected mongodb and made server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const mongoose = require('mongoose')
+const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.use(express.json())
+
+let connection = mongoose.connect("mongodb+srv://malvan:fiba8830@malvan.c6y6w.mongodb.net/ecomdb")
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+
+app.post("/create",async(req,res)=>{
+  let payload = req.body;
+  try{
+      let new_user = new userModel(payload);
+      await new_user.save();
+  }catch(err){
+    res.send({"error":err})
+}})
+
+
+app.listen(PORT,async()=>{
+  try{
+      await connection;
+      console.log("Successfully connected to MongoDB");
+  } catch (error){
+      console.log(error);
+  }
+  
+      console.log(`Server is running on port ${PORT}`)
+  })
